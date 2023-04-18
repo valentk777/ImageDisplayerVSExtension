1b18c342edf12abe50a367897d3d46a798c2eb95
Valentinas <31697821+valentk777@users.noreply.github.com>
2023-04-18 07:36:25 +0300

    Visual studio theme added

--- ImageDisplayerVSExtension/ToolWindows/ImageDisplayWindow.cs
+++ ImageDisplayerVSExtension/ToolWindows/ImageDisplayWindow.cs
@@ -23,7 +23,7 @@ namespace ImageDisplayer.ToolWindows
         /// </summary>
         public ImageDisplayWindow() : base(null)
         {
-            Caption = "ImageDisplayWindow";
+            Caption = "Display Image";
 
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
